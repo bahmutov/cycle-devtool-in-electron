fdcf0a488a16ebd1507bed8eeb56d1d59fe30c3c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-04-22 15:44:52 +0200

    working cycle devtool extension

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const electron = require('electron');
 const loadDevtool = require('electron-load-devtool');
 
 electron.app.on('ready', () => {
-  const win = new electron.BrowserWindow({width: 400, height: 400});
+  const win = new electron.BrowserWindow({width: 1000, height: 500});
   win.loadURL(`file://${__dirname}/index.html`);
 
-  loadDevtool(loadDevtool.VUEJS_DEVTOOLS);
+  const CycleDevToolId = 'dfgplfmhhmdekalbpejekgfegkonjpfp'
+  // loadDevtool(loadDevtool.VUEJS_DEVTOOLS);
+  loadDevtool(CycleDevToolId)
 
   win.openDevTools();
 });
